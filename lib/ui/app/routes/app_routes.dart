b3f72989ae4105a1db475cfe28dae5fa98861e83
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2022-04-02 10:29:28 -0500

    chore: add ProductsPage to route

--- lib/ui/app/routes/app_routes.dart
+++ lib/ui/app/routes/app_routes.dart
@@ -1,12 +1,13 @@
 import 'package:app_catalogo/ui/app/bloc/app_bloc.dart';
 import 'package:app_catalogo/ui/counter/view/counter_page.dart';
+import 'package:app_catalogo/ui/products/presentation/view/products_page.dart';
 import 'package:flutter/material.dart';
 
 List<Page> onGenerateAppViewPages(AppState state, List<Page> pages) {
   switch (state.pageStatus) {
     case AppPageStatus.home:
       return [
-        CounterPage.page(),
+        ProductsPage.page(),
       ];
     case AppPageStatus.traking:
       return [
@@ -14,7 +15,7 @@ List<Page> onGenerateAppViewPages(AppState state, List<Page> pages) {
       ];
     case AppPageStatus.profile:
       return [
-        CounterPage.page(),
+        ProductsPage.page(),
       ];
   }
 }
